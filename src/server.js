bf3b6e9ef082fef4b87601e15b500a718b2fc782
corxl <reifservis@me.com>
2024-01-12 17:26:01 -0800

    added test request for user using .rest file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import http from 'http';
 
 
 const app = express(); 
 const parser = bodyParser.json(); 
+
+app.use(parser); 
+
+app.use('/users', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) { 
+        res.status(400).send('Incorrect payload'); 
+    }
+    res.send(`User ${email} logged in with password ${password}`);
+});
+
+
+http.createServer(app).listen(3000, () => {
+    console.log('Listening on port 3000');
+});
